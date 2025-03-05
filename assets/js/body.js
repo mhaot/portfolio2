00f6f1d079c2b9ad16a92dfd2ec835c1394ec306
‘aota201507071020’ <aota20150707.1020@gmail.com>
2025-03-06 00:00:27 +0900

    3/5

--- assets/js/body.js
+++ assets/js/body.js
@@ -30,6 +30,22 @@ export function initBody() {
     });
 
 
+// l-header__navMenu--open&close
+    const navBtn = document.querySelector('#js-navBtn');
+    const navMenu = document.querySelector('#js-navMenu');
+    const header = document.querySelector('.l-header');
+
+    navBtn.addEventListener('click', function () {
+        // ナビボタンとナビメニューのactiveクラスをトグル
+        navBtn.classList.toggle('active');
+        navMenu.classList.toggle('active');
+
+        // ヘッダーにactiveクラスをトグルして、横幅の変更を反映
+        header.classList.toggle('active');
+
+    });
+
+
 // mainVisual__slide
     let currentSlide = 0;
     const slides = document.querySelectorAll('.slideImg--item');
@@ -60,21 +76,31 @@ export function initBody() {
     setInterval(changeSlide, 8000);
     updateSlideAndIndicator();}
 
+// メイン画像の下部が画面に見えたらボタンを表示
+function toggleScrollEvent() {
+    const buttonBnr = document.querySelector('.l-onlineStore__bnr');
+    if (!buttonBnr) return;
 
+    if (window.innerWidth > 959) {
+    buttonBnr.style.display = 'none';
+        window.removeEventListener('scroll', scrollHandler);
+    } else {
+        window.addEventListener('scroll', scrollHandler);
+    }
+}
 
+    function scrollHandler() {
+        const mainSlide = document.querySelector('.mainVisual__slide');
+        const buttonBnr = document.querySelector('.l-onlineStore__bnr');
 
+    if (!mainSlide || !buttonBnr) return;
 
+    if (window.scrollY >= mainSlide.offsetHeight) {
+        buttonBnr.style.display = 'block';
+    } else {
+        buttonBnr.style.display = 'none';
+    }
+}
 
-    // l-header__navMenu--open&close
-const navBtn = document.querySelector('#js-navBtn');
-const navMenu = document.querySelector('#js-navMenu');
-const header = document.querySelector('.l-header'); // ヘッダーの要素を追加
-
-navBtn.addEventListener('click', function () {
-    // ナビボタンとナビメニューのactiveクラスをトグル
-    navBtn.classList.toggle('active');
-    navMenu.classList.toggle('active');
-
-    // ヘッダーにactiveクラスをトグルして、横幅の変更を反映
-    header.classList.toggle('active');
-});
+    window.addEventListener('resize', toggleScrollEvent);
+    window.addEventListener('DOMContentLoaded', toggleScrollEvent);
