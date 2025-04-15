5a8cf6f24ad7dbe714446aada2f02235f3bf470a
‘aota201507071020’ <aota20150707.1020@gmail.com>
2025-04-15 21:12:46 +0900

    4/15

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,15 +1,5 @@
 // apps.js
 import { initBody } from './body.js';
-// import { initMain } from './main.js';
-// import { initCarousel } from './carousel.js';
-// import { initProducts } from './products.js';
-// import { initAbout } from './about.js';
-// import { initMemberShip } from './memberShip.js';
 
 
     initBody();
-//     initMain();
-//     initCarousel();
-//     initProducts();
-//     initAbout();
-//     initMemberShip();
